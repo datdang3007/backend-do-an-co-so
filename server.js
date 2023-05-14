6855a9f269d9df677397370fcb0d86b5f954315e
datdang3007 <datdang3007@gmail.com>
2023-05-14 22:57:38 +0700

    set limit upload

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors());
 
 app.use("/api", user);
